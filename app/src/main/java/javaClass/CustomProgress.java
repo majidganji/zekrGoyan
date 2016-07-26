f3e9ccdea26622620fe5807eb9f1732874539670
majid ganji <majidganji313@gmail.com>
2016-07-26 22:51:54 +0430

    add alertdialog complite a 100 count zekr

---
+++ app/src/main/java/javaClass/CustomProgress.java
@@ -0,0 +1,82 @@
+package javaClass;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+public class CustomProgress extends ImageView {
+
+    private final int STROKE_WIDTH = 8;
+    private Paint paint;
+    private Paint textPaint;
+    private int percent = 98;
+
+    public CustomProgress(Context context) {
+        super(context);
+        initialize(context);
+    }
+
+    public CustomProgress(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initialize(context);
+    }
+
+    public CustomProgress(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initialize(context);
+    }
+
+    public CustomProgress(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        initialize(context);
+    }
+
+    private void initialize(Context context){
+        paint = new Paint();
+        paint.setColor(Color.RED);
+        paint.setAntiAlias(true);
+        paint.setStrokeWidth(STROKE_WIDTH);
+        paint.setStyle(Paint.Style.STROKE);
+
+        textPaint = new Paint();
+        textPaint.setColor(Color.GREEN);
+        textPaint.setAntiAlias(true);
+        textPaint.setTextAlign(Paint.Align.CENTER);
+        textPaint.setTextSize(35);
+        textPaint.setStyle(Paint.Style.FILL_AND_STROKE);
+    }
+
+    public void setPercent(int value){
+        percent = value;
+        postInvalidate();
+    }
+
+    public int getPercent(){
+        return percent;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        RectF rect = new RectF();
+        rect.left = 0 + STROKE_WIDTH;
+        rect.right = getWidth() - STROKE_WIDTH;
+        rect.top = 0 + STROKE_WIDTH;
+        rect.bottom = getHeight() - STROKE_WIDTH;
+
+        int sweepAngle = percent * 360 / 100;
+
+        int red = (int )((100 - percent) * 2.5f);
+        int green = (int)(percent * 2.5f);
+        int bule = 0;
+        paint.setColor(Color.rgb(red, green, bule));
+
+        canvas.drawArc(rect, -90, sweepAngle, false, paint);
+        canvas.drawText(String.valueOf(percent) , getWidth() / 2, getHeight() / 2, textPaint);
+    }
+}
--- app/src/main/java/majid/ganji/zekrgoyan/CountActivity.java
+++ app/src/main/java/majid/ganji/zekrgoyan/CountActivity.java
@@ -2,22 +2,34 @@ package majid.ganji.zekrgoyan;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
+import cn.pedant.SweetAlert.SweetAlertDialog;
+import javaClass.CustomProgress;
 import javaClass.Week;
 
 public class CountActivity extends AppCompatActivity {
 
-    TextView txtCount;
+    CustomProgress cusCount;
+    TextView txtZekr;
+    ViewGroup lytCount1;
+    ViewGroup lytCount2;
+
     private ArrayList<Week> week = new ArrayList<Week>();
     private String[] days = {"شنبه", "یکشنبه", "دوشنبه", "سه شنبه", "چهارشنبه", "پنجشنبه", "جمعه"};
-    private String[] zekrs = {"شنبه", "یکشنبه", "دوشنبه", "سه شنبه", "چهارشنبه", "پنجشنبه", "جمعه"};
+    private String[] zekrs = {"یا رب العالمین", "یا ذالجلال والاکرام", "یا قاضی الحاجات", "یا ارحم الراحمین",
+            "یا حی یا قیوم", "لا اله الا الله الملک الحق المبین", "اللهم صل علی محمد و ال محمد"};
 
-    private void initialize(){
-        txtCount = (TextView) findViewById(R.id.txtCount);
+    private void initialize() {
+        txtZekr = (TextView) findViewById(R.id.txtZekr);
+        cusCount = (CustomProgress) findViewById(R.id.cusCount);
+        lytCount1 = (ViewGroup) findViewById(R.id.lytCount1);
+        lytCount2 = (ViewGroup) findViewById(R.id.lytCount2);
     }
 
     @Override
@@ -26,25 +38,60 @@ public class CountActivity extends AppCompatActivity {
         setContentView(R.layout.activity_count);
 
         Bundle bundle = getIntent().getExtras();
-        if (!bundle.containsKey("day")){
+        if (!bundle.containsKey("day")) {
             finish();
         }
         int day = bundle.getInt("day");
 
         initialize();
 
-        for(int i = 0; i < 7; i++){
+        for (int i = 0; i < 7; i++) {
             Week week = new Week();
             week.number = i;
             week.name = days[i];
             week.zekr = zekrs[i];
             this.week.add(week);
         }
+        getDay(day);
 
-        Toast.makeText(CountActivity.this, days[0], Toast.LENGTH_SHORT).show();
+        cusCount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                click();
+            }
+        });
+
+        lytCount1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                click();
+            }
+        });
+
+        lytCount2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                click();
+            }
+        });
     }
 
-    private void getDay(int numberDay){
+    private void getDay(int numberDay) {
+        txtZekr.setText(this.week.get(numberDay).zekr);
+    }
 
+    private void click(){
+        int count = cusCount.getPercent();
+        if (count < 100){
+            cusCount.setPercent(++count);
+            if (count == 100){
+                //TODO:: Alert message for complite zekr
+                new SweetAlertDialog(this)
+                        .setTitleText("ماشالله")
+                        .setContentText("ذکر امروز تموم شد.")
+                        .setConfirmText("خدایا شکر")
+                        .show();
+            }
+        }
     }
 }
